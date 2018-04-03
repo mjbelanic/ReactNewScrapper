92e1d3fff1e88aa5bee9993b78d9c48d693c800f
mjbelanic <mjbelanic@aol.com>
2018-04-03 09:36:44 -0800

    heroku edits

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const db = mongoose.connection;
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(__dirname));
 	const path = require("path");
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
