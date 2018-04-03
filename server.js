d4211a193f8704a7e178cdcfdb0ef82476ffdcf5
mjbelanic <mjbelanic@aol.com>
2018-04-03 10:07:44 -0800

    edits

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ const db = mongoose.connection;
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(__dirname));
-	const path = require("path");
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname);
-	});
+	app.use(express.static(__dirname + "/client/build"));
 }
 
 const PORT = process.env.PORT || 5000;
