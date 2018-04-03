f5a68ba718a05770a501e10b092d37d2a962fa1f
mjbelanic <mjbelanic@aol.com>
2018-04-03 10:52:38 -0800

    edits

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ const db = mongoose.connection;
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(__dirname + "/client/build"));
+	app.use(express.static("client/build"));
+
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
 
 const PORT = process.env.PORT || 5000;
