9b01cc2af4f7cc347cdeb4e0d96ce7c853e87db1
mjbelanic <mjbelanic@aol.com>
2018-04-03 10:56:54 -0800

    edits

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ const db = mongoose.connection;
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-
 	const path = require("path");
+	app.use(express.static(path.join(__dirname, "/client/build")));
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
