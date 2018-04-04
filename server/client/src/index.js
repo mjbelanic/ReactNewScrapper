28cbb693f6e1e452d0357c9ffbe0057987f504ac
mjbelanic <mjbelanic@aol.com>
2018-04-04 15:18:48 -0800

    removed assets folder, moved file into  index.css

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,5 @@
 import "materialize-css/dist/css/materialize.min.css";
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -13,5 +14,5 @@ ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>,
-	document.querySelector("#root")
+	document.getElementById("root")
 );
