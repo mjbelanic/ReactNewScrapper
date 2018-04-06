cbb12b68dee382c84e9ed244e6f2978fb2548562
mjbelanic <mjbelanic@aol.com>
2018-04-05 16:26:49 -0800

    fixed error in index.js const 3app to const app

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("./models/Articles");
 require("./models/Comments");
-const 3app = express();
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -18,8 +18,10 @@ mongoose.connect(
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
-	const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "client", "build", "index.html")));
+  const path = require("path");
+  app.use(
+    express.static(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
