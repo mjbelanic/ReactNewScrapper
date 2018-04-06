9ba7244513afb276244e1333fc962b80e8b601a4
mjbelanic <mjbelanic@aol.com>
2018-04-06 09:27:58 -0800

    edit index.html

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -10,20 +10,18 @@ import { connect } from "react-redux";
 import * as actions from "./actions";
 
 class App extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route exact={true} path="/" component={Instructions} />
-					<Route exact path="/articles" component={Articles} />
-					<Route exact path="/saved" component={SavedArticles} />
-					<Route exact path="/:id/comments" component={ArticleComments} />
-					<Route exact path="/:id/newComment" component={NewCommentForm} />
-				</div>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Route exact={true} path="/" component={Instructions} />
+        <Route exact path="/articles" component={Articles} />
+        <Route exact path="/saved" component={SavedArticles} />
+        <Route exact path="/:id/comments" component={ArticleComments} />
+        <Route exact path="/:id/newComment" component={NewCommentForm} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default connect(null, actions)(App);
