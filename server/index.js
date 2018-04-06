feef4b5bbac31a243a49a6c20ea83ae37241244b
mjbelanic <mjbelanic@aol.com>
2018-04-06 00:10:18 -0800

    route  test

--- server/index.js
+++ server/index.js
@@ -18,11 +18,11 @@ mongoose.connect(
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.send("hi");
   });
-  // const path = require("path");
-  // app.use(express.static(path.join(__dirname, "client/build")));
   //
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
