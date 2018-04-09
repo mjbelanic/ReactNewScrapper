c6fafb3dd8ebeef4ffa3516b51682e13225f8f51
mjbelanic <mjbelanic@aol.com>
2018-04-08 20:15:42 -0800

    remove script tags from index.html

--- server/index.js
+++ server/index.js
@@ -5,22 +5,19 @@ require("./models/Articles");
 require("./models/Comments");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/ReactNewScrapper",
-  {
-    useMongoClient: true
-  }
+  { useMongoClient: true }
 );
 
 require("./routes/index")(app);
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   const path = require("path");
-  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
