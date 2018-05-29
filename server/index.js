79bfa144a9d0a10544d173cf81aa91b8c6d4e9de
mjbelanic <mjbelanic@aol.com>
2018-05-29 13:00:46 -0800

    altered server file

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname+"build/index.html"));
+    res.sendFile(path.resolve(__dirname+"/build/index.html"));
   });
 }
 const PORT = process.env.PORT || 5000;
